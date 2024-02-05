aef0deb3463e86eb2aa1b5d72647a5c03823748a
Johannes Günter Herforth <johannes.herforth@uni.lu>
2024-02-05 16:58:05 +0100

    added functionality of taking input vertices and creating new blend shape vertices off of it

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,149 @@
-use std::{io::{BufWriter, Write}, fs::File};
+use std::{io::{BufWriter, Write}, fs::File, collections::HashMap};
 
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 use fbx::{self, Node, Property};
 
 fn main() {
-    let file = std::fs::File::open("sample.fbx").expect("Failed to open file");
+    // Define constants
+    // const NEWSHAPENAME: &'static str = "cprint";
+    const NEWSHAPENAME: &'static str = "BodyHeavy";
 
+    // Load the vertex file
+    let file = std::fs::File::open("sample-verts.json").expect("Failed to open file");
+    let reader = std::io::BufReader::new(file);
+    let render_meshes: RendererMeshes = serde_json::from_reader(reader).unwrap();
+
+
+    // load the fbx file
+    let file = std::fs::File::open("sample.fbx").expect("Failed to open file");
     let reader = std::io::BufReader::new(file);
     let file = fbx::File::read_from(reader).unwrap();
-    
-    let emaciated_geometry: Vec<&Node> = file.children[8].children.iter().filter(|x| {if let Property::String(name) = &x.properties[1] {name.contains("BodyHeavy") && x.name == "Geometry"} else {false}}).collect::<Vec<&Node>>();
-    
-    let main_mesh: &Node = &file.children[8].children[0];
 
-    println!("{:?}", main_mesh.children[0].properties);
-
-    match &main_mesh.children[0].properties[0] {
-        Property::F64Array(a) => {println!("{:?}", a.len());},
-        _ => {}
-    }
-
+    // Extract wanted blendshapes from the file
     let mut new_blendshapes: Vec<BlendShape> = vec![];
-
+    let emaciated_geometry: Vec<&Node> = file.children[8].children.iter().filter(|x| {if let Property::String(name) = &x.properties[1] {name.contains(NEWSHAPENAME) && x.name == "Geometry" && !name.contains("cbs") && !name.contains("Tear") && !name.contains("Mouth") && !name.contains("Eyelashes") && !name.contains("Eyes")} else {false}}).collect::<Vec<&Node>>();
     for n in emaciated_geometry.iter() {
+        if let Property::F64Array(a) = &n.children[2].properties[0] {
+            println!("{:?}", a.len())
+        }
         new_blendshapes.push(BlendShape::new(n))
     }
 
-    let blendshape_holder:BlendShapes = BlendShapes { shapes: new_blendshapes };
+    let mut blendshape_holder:BlendShapes = BlendShapes { shapes: new_blendshapes };
+    // blendshape_holder.shapes.iter().for_each(|x| println!("{}", x.name));
 
-    let output_file = File::create("sample.json").unwrap();
+
+
+    // println!("{:?}", emaciated_geometry);
+
+    // emaciated_geometry.iter()
+
+    
+    // let main_mesh: &Node = &file.children[8].children[0];
+    // println!("{:?}", emaciated_geometry.len());
+
+    // if let Property::F64Array(a) = &main_mesh.children[0].properties[0] {
+    //     println!("{:?}", a.len())
+    // }
+    // for geo in render_meshes.meshes.iter() {
+        // let mut new_blend = SkinnedMeshRenderer::new(format!("{}{}", geo.name.replace(".Shape", "__"),  NEWSHAPENAME), geo.vertices.len());
+        
+        // let mut new_blend = BlendShape::default();
+        // new_blend.name = format!("{}{}", geo.name.replace(".Shape", "__"),  NEWSHAPENAME);
+        // new_blend.vertices = Vec::with_capacity(geo.vertices.len());
+
+        // let mut new_blends: Vec<f64> = Vec::with_capacity(geo.vertices.len() - );
+
+
+
+        // println!("{}, {}, {:?}", new_blend.name, new_blend.vertices.len(), );
+
+    // }
+
+    // create the new blendshape vertices and indices
+    for shape in blendshape_holder.shapes.iter_mut() {
+        let related_geometry = render_meshes.meshes.iter().find(|x| shape.name.contains(&x.name.replace(".Shape", ""))).unwrap();
+        let mut new_verts: Vec<f64> = Vec::with_capacity(related_geometry.vertices.len() - *shape.indices.last().unwrap() as usize);
+
+        let highest_index: usize = *shape.indices.last().unwrap() as usize;
+
+        let matching = find_matching_indices(&related_geometry.vertices[..=highest_index], &related_geometry.vertices[(highest_index + 1)..]);
+
+        for i in 0..new_verts.len() {
+            new_verts[i] = shape.vertices[*matching.get(&(i as i64)).unwrap().first().unwrap() as usize];
+        }
+
+        shape.vertices.append(&mut new_verts);
+        shape.indices.extend::<Vec<i64>>((highest_index as i64..related_geometry.vertices.len() as i64).collect());
+    }
+
+    // export new file
+    let output_file = File::create("sample_new.json").unwrap();
     let mut writer = BufWriter::new(output_file);
     serde_json::to_writer(&mut writer, &blendshape_holder).unwrap();
     writer.flush().unwrap();
 }
+
+fn find_matching_indices(array: &[Vector3], to_match: &[Vector3]) -> HashMap<i64, Vec<i64>> {
+    let mut index_map: HashMap<i64, Vec<i64>> = HashMap::with_capacity(to_match.len());
+
+    for i in 0..to_match.len() {
+        index_map.insert(i as i64, vec![]);
+    }
+
+    array.iter()
+         .enumerate()
+         .for_each(|(i, x)| {
+            to_match.iter()
+                    .enumerate()
+                    .for_each(|(j, y)| {
+                        if x == y {
+                            index_map.get_mut(&(j as i64)).unwrap().push(i as i64);
+                        }
+                    });
+         });
+
+    index_map
+}
+
+
+#[derive(Deserialize, Debug, Default)]
+struct RendererMeshes {
+    meshes: Vec<SkinnedMeshRenderer>
+}
+
+#[derive(Deserialize, Debug, Default)]
+struct SkinnedMeshRenderer {
+    name: String,
+    vertices: Vec<Vector3>
+}
+
+// impl SkinnedMeshRenderer {
+//     pub fn new(name: String, vec_len: usize) -> SkinnedMeshRenderer {
+//         SkinnedMeshRenderer { name, vertices: Vec::with_capacity(vec_len) }
+//     }
+// }
+
+
+
+#[derive(Deserialize, Debug, Default, PartialEq)]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+// impl Vector3 {
+//     pub fn new(x: f64, y: f64, z: f64) -> Vector3 {
+//         Vector3 { x, y, z }
+
+//     }
+
+//     pub fn equals(&self, other: &Vector3) -> bool {
+//         self.x == other.x && self.y == other.y && self.z == other.z
+//     }
+// }
 
 
 #[derive(Serialize, Debug, Default)]
@@ -55,9 +165,14 @@ impl BlendShape {
     pub fn new(input_node: &Node) -> BlendShape {
         let id: i64 = Self::extract_i64(&input_node.properties[0]).unwrap();
         let name: String = Self::extract_string(&input_node.properties[1]).unwrap().replace("\0\u{1}Geometry", "");
-        let indices: Vec<i64> = Self::extract_i64_array(&input_node.children[1].properties[0]).unwrap();
-        let vertices: Vec<f64> = Self::extract_f64_array(&input_node.children[2].properties[0]).unwrap();
-        let normals: Vec<f64> = Self::extract_f64_array(&input_node.children[3].properties[0]).unwrap();
+        let indices: Vec<i64> = Self::extract_i64_array(&input_node.children[1].properties[0]).unwrap_or_default();
+        let vertices: Vec<f64> = Self::extract_f64_array(&input_node.children[2].properties[0]).unwrap_or_default();
+        let normals: Vec<f64>;
+        if input_node.children.len() == 4 {
+            normals = Self::extract_f64_array(&input_node.children[3].properties[0]).unwrap_or_default();
+        } else {
+            normals = vec![];
+        }
         
         BlendShape { id, name, indices, vertices, normals }
     }

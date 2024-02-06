a9ccfd618de17b6c9d7e51cc54f7f6858e1949b0
DerAndereJohannes <johannes.herforth@uni.lu>
2024-02-06 18:03:34 +0100

    altered code to produce already Vector3 output for the vertices

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::{io::{BufWriter, Write}, fs::File, collections::HashMap};
-
 use serde::{Serialize, Deserialize};
-
 use fbx::{self, Node, Property};
 
 fn main() {
     // Define constants
-    // const NEWSHAPENAME: &'static str = "cprint";
     const FBXFILENAME: &'static str = "sample.fbx";
     const VERTEXFILENAME: &'static str = "sample-verts.json";
-    const NEWSHAPENAME: &'static str = "BodyHeavy";
+    const BLENDSHAPEFILENAME: &'static str = "cprint-blendshapes.json";
+    const OLDSHAPENAME: &'static str = "BodyHeavy";
+    const NEWSHAPENAME: &'static str = "cprint";
 
     // Load the vertex file
     let file = std::fs::File::open(VERTEXFILENAME).expect("Failed to open file");
@@ -27,7 +26,7 @@ fn main() {
     file.children[8].children.iter()
                              .filter(|x| {
                                  if let Property::String(name) = &x.properties[1] {
-                                     name.contains(NEWSHAPENAME) && 
+                                     name.contains(OLDSHAPENAME) && 
                                          x.name == "Geometry" && 
                                          !name.contains("cbs") && 
                                          !name.contains("Tear") && 
@@ -37,31 +36,31 @@ fn main() {
                                  } else {
                                      false
                                  }
-                             }).for_each(|x| new_blendshapes.push(BlendShape::new(x)));
+                             }).for_each(|x| new_blendshapes.push(BlendShape::new(x, OLDSHAPENAME, NEWSHAPENAME)));
 
-    let mut blendshape_holder:BlendShapes = BlendShapes { shapes: new_blendshapes };
+    let blendshape_holder:BlendShapes = BlendShapes { blendshapes: new_blendshapes };
+    let mut unity_blendshapes: UnityBlendShapes = UnityBlendShapes { meshes: vec![] };
 
-    // create the new blendshape vertices and indices
-    for shape in blendshape_holder.shapes.iter_mut() {
+    // create the new blendshape vertices
+    for shape in blendshape_holder.blendshapes.iter() {
         let related_geometry = render_meshes.meshes.iter().find(|x| shape.name.contains(&x.name.replace(".Shape", ""))).unwrap();
-        let mut new_verts: Vec<f64> = Vec::with_capacity(related_geometry.vertices.len() - *shape.indices.last().unwrap() as usize);
-
+        let mut new_shape = ExchangeShapeReference::new(format!("{}__{}", related_geometry.name.replace(".Shape", ""), NEWSHAPENAME), &shape, related_geometry.vertices.len());
         let highest_index: usize = *shape.indices.last().unwrap() as usize;
 
         let matching = find_matching_indices(&related_geometry.vertices[..=highest_index], &related_geometry.vertices[(highest_index + 1)..]);
-
-        for i in 0..new_verts.len() {
-            new_verts[i] = shape.vertices[*matching.get(&(i as i64)).unwrap().first().unwrap() as usize];
+        for (key, value) in matching.iter() {
+            if value.len() > 0 {
+                let first_value: &Vector3 = &new_shape.vertices[*value.first().unwrap() as usize].clone();
+                new_shape.vertices[*key as usize + new_shape.cutoff].update(first_value.x, first_value.y, first_value.z);
+            }
         }
-
-        shape.vertices.append(&mut new_verts);
-        shape.indices.extend::<Vec<i64>>((highest_index as i64..related_geometry.vertices.len() as i64).collect());
+        unity_blendshapes.meshes.push(new_shape);
     }
 
     // export new file
-    let output_file = File::create("sample_new.json").unwrap();
+    let output_file = File::create(BLENDSHAPEFILENAME).unwrap();
     let mut writer = BufWriter::new(output_file);
-    serde_json::to_writer(&mut writer, &blendshape_holder).unwrap();
+    serde_json::to_writer(&mut writer, &unity_blendshapes).unwrap();
     writer.flush().unwrap();
 }
 
@@ -100,16 +99,50 @@ struct SkinnedMeshRenderer {
 }
 
 
-#[derive(Deserialize, Debug, Default, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone,  Default, PartialEq)]
 struct Vector3 {
     x: f64,
     y: f64,
     z: f64
 }
 
+impl Vector3 {
+    pub fn update(&mut self, x: f64, y: f64, z: f64) {
+        self.x = x;
+        self.y = y;
+        self.z = z;
+    }
+}
+
 #[derive(Serialize, Debug, Default)]
 struct BlendShapes {
-    shapes: Vec<BlendShape>
+    blendshapes: Vec<BlendShape>
+}
+
+
+#[derive(Serialize, Debug, Default)]
+struct UnityBlendShapes {
+    meshes: Vec<ExchangeShapeReference>
+}
+
+#[derive(Serialize, Debug, Default)]
+struct ExchangeShapeReference {
+    name: String,
+    #[serde(skip_serializing)]
+    cutoff: usize,
+    vertices: Vec<Vector3>
+}
+
+impl ExchangeShapeReference {
+    pub fn new(name: String, blendshape: &BlendShape, vertex_count: usize) -> ExchangeShapeReference {
+        let mut vertices = vec![Vector3::default(); vertex_count];
+
+        for (i, index) in blendshape.indices.iter().enumerate() {
+            let start: usize = i * 3;
+            vertices[*index as usize].update(-blendshape.vertices[start], blendshape.vertices[start + 1], blendshape.vertices[start + 2]); 
+        }
+        ExchangeShapeReference { name, cutoff: *blendshape.indices.last().unwrap() as usize + 1, vertices }
+    }
 }
 
 #[derive(Serialize, Debug, Default)]
@@ -123,9 +156,9 @@ struct BlendShape {
 
 impl BlendShape {
 
-    pub fn new(input_node: &Node) -> BlendShape {
+    pub fn new(input_node: &Node, old_shape_name: &str, new_shape_name: &str) -> BlendShape {
         let id: i64 = Self::extract_i64(&input_node.properties[0]).unwrap();
-        let name: String = Self::extract_string(&input_node.properties[1]).unwrap().replace("\0\u{1}Geometry", "");
+        let name: String = Self::extract_string(&input_node.properties[1]).unwrap().replace("\0\u{1}Geometry", "").replace(old_shape_name, new_shape_name);
         let indices: Vec<i64> = Self::extract_i64_array(&input_node.children[1].properties[0]).unwrap_or_default();
         let vertices: Vec<f64> = Self::extract_f64_array(&input_node.children[2].properties[0]).unwrap_or_default();
         let normals: Vec<f64>;
